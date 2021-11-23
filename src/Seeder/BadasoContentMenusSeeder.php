c195e85e2acb810909e906468646d6aafd60f1d1
Rizki Heryandi <rizkiheryandi@users.noreply.github.com>
2021-11-23 09:07:37 +0000

    Apply fixes from StyleCI

--- src/Seeder/BadasoContentMenusSeeder.php
+++ src/Seeder/BadasoContentMenusSeeder.php
@@ -10,9 +10,9 @@ class BadasoContentMenusSeeder extends Seeder
     /**
      * Auto generated seed file.
      *
-     * @throws Exception
-     *
      * @return void
+     *
+     * @throws Exception
      */
     public function run()
     {
