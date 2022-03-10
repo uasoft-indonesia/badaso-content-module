be256011b5451b7e5d4525e0a97ee01407421eeb
Rizki Heryandi <rizkiheryandi@gmail.com>
2022-03-10 13:37:58 +0700

    tes

--- tests/Feature/BadasoContentApiTest.php
+++ tests/Feature/BadasoContentApiTest.php
@@ -213,6 +213,7 @@ class BadasoContentApiTest extends TestCase
             $response->assertSuccessful();
             
         $table = Content::latest()->first();
+        $table = json_decode($table, true);
         foreach ($table as $key => $value) {
             dd($key, $value, $table->value);
             if($table->value == $key){
