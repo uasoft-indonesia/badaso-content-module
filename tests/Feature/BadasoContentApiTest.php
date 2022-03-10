9554615a9999dfa3f923e2c22d7a88303b0464ae
Rizki Heryandi <rizkiheryandi@gmail.com>
2022-03-10 14:28:28 +0700

    tes

--- tests/Feature/BadasoContentApiTest.php
+++ tests/Feature/BadasoContentApiTest.php
@@ -217,7 +217,13 @@ class BadasoContentApiTest extends TestCase
         $request_data = json_decode($request_data['value'], true);
         foreach ($table as $key => $value) {
             if($request_data[$key]){
-                dd($request_data[$key]['name'],$value);
+                $this->assertTrue($request_data[$key]['name'] == $value->name);
+                $this->assertTrue($request_data[$key]['type'] == $value->type);
+                $this->assertTrue($request_data[$key]['label'] == $value->label);
+                if ($value->data['url']) {
+                    $this->assertTrue($request_data[$key]['data']['url'] == $value->data['url']);
+                    $this->assertTrue($request_data[$key]['data']['text'] == $value->data['text']);
+                }
             }
         }
 
