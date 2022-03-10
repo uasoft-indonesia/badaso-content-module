fe3157d0b4976b60e5ed8af3f9f4a63f044b0369
Rizki Heryandi <rizkiheryandi@gmail.com>
2022-03-10 16:41:14 +0700

    test2

--- tests/Feature/BadasoContentApiTest.php
+++ tests/Feature/BadasoContentApiTest.php
@@ -366,6 +366,7 @@ class BadasoContentApiTest extends TestCase
                         }
                     }
                 } else {
+                    dd($value,$tab);
                     $respon_data_array = $tab[$key];
                     if (isset($respon_data_array['data'])) {
                         if (isset($respon_data_array['data']['url']) && isset($tab['data']['url'])) {
